3e715056457ae389e7247594dc9470b566245193
Baturpcu <batusalkim11@gmail.com>
2022-05-30 03:15:13 +0300

    aaa
    aaaa

---
+++ WebApplication1/ViewModel/DerslerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModel
+{
+    public class DerslerModel
+    {
+        public int dersId { get; set; }
+        public string dersKodu { get; set; }
+        public string dersAdi { get; set; }
+        public string dersKredi { get; set; }
+    }
+}
